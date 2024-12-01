c4cfb83fdddcdb30c5edff58cba9b5125bed430c
Abid <abidroid1@gmail.com>
2024-12-01 10:50:18 +0500

    stack

--- lib/screens/first_screen.dart
+++ lib/screens/first_screen.dart
@@ -7,7 +7,7 @@ class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Text headingText = Text(
-      "Welcome to confusion",
+      "Welcome to AKTI",
       style: TextStyle(
         color: Colors.white,
         fontSize: 40,
@@ -20,26 +20,24 @@ class FirstScreen extends StatelessWidget {
       height: 200,
       margin: EdgeInsets.only(left: 100, top: 100),
       padding: EdgeInsets.only(left: 20, top: 20),
-
       decoration: BoxDecoration(
-
-        border: Border.all(color: Colors.yellow, width: 5,),
-        borderRadius: BorderRadius.circular(20),
-
-        gradient: LinearGradient(colors: [
-          Colors.red, Colors.amber, Colors.purple],
-
-          begin: Alignment.topLeft,
-          end: Alignment.bottomCenter,
-        )
-
+          border: Border.all(
+            color: Colors.yellow,
+            width: 5,
+          ),
+          borderRadius: BorderRadius.circular(20),
+          gradient: LinearGradient(
+            colors: [Colors.red, Colors.amber, Colors.purple],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomCenter,
+          )),
+      child: Text(
+        'Dibba',
+        style: TextStyle(color: Colors.white),
       ),
-
-      child: Text('Dibba', style: TextStyle(color: Colors.white),),
     );
 
     return Scaffold(
-      backgroundColor: Colors.green,
       appBar: AppBar(
         backgroundColor: Colors.brown,
         title: Text(
@@ -47,7 +45,35 @@ class FirstScreen extends StatelessWidget {
           style: TextStyle(color: Colors.white, fontSize: 40),
         ),
       ),
-      body: box,
+      body: Stack(
+        alignment: Alignment.center,
+        clipBehavior: Clip.none,
+        children: [
+          Container(
+            width: 300,
+            height: 300,
+            color: Colors.red,
+          ),
+          Container(
+            width: 200,
+            height: 200,
+            color: Colors.amber,
+          ),
+
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.blue,
+          ),
+
+          Positioned(
+              bottom: -25,
+              right: 20,
+              child: Container(width: 50, height: 50, color: Colors.yellow,)),
+
+
+        ],
+      ),
     );
   }
 }
